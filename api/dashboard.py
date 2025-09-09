31dc3710950671daaf825c1091c32655d02aacad
Ihor Savenko <csigorek@gmail.com>
2025-09-09 23:32:49 +0100

    fix import

--- api/dashboard.py
+++ api/dashboard.py
@@ -1,9 +1,8 @@
-from h11 import Request
 from app.core.jwt import DecodedToken, FastJWT
 from models.models import User
 from app.core.password_utils import get_password_hash, verify_password
 from beanie import PydanticObjectId
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from pydantic import BaseModel, Field
 
 dashboard_router = APIRouter(prefix="/dashboard")
@@ -13,7 +12,6 @@ async def dashboard_event(request: Request):
     token: DecodedToken = await FastJWT().decode(request.headers["Authorization"])
     user = await User.get(token.id)
     if not user:
-        print(token, user)
         raise HTTPException(401, "Unauthorized")
 
 
